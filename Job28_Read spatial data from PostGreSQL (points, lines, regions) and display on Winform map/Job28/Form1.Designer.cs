9d17e6ccc78fb4473f83bc5ed46ca8206e134be1
DungSatoru <dungsatoru.dev@gmail.com>
2024-04-27 15:43:31 +0700

    Job28_Read spatial data from PostGreSQL (points, lines, regions) and display on Winform map

---
+++ Job28_Read spatial data from PostGreSQL (points, lines, regions) and display on Winform map/Job28/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Job28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbPoint = new System.Windows.Forms.RadioButton();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.rbPolygon = new System.Windows.Forms.RadioButton();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.panelMap = new GMap.NET.WindowsForms.GMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbPoint
+            // 
+            this.rbPoint.AutoSize = true;
+            this.rbPoint.Checked = true;
+            this.rbPoint.Location = new System.Drawing.Point(12, 12);
+            this.rbPoint.Name = "rbPoint";
+            this.rbPoint.Size = new System.Drawing.Size(58, 20);
+            this.rbPoint.TabIndex = 0;
+            this.rbPoint.TabStop = true;
+            this.rbPoint.Text = "Point";
+            this.rbPoint.UseVisualStyleBackColor = true;
+            // 
+            // rbLine
+            // 
+            this.rbLine.AutoSize = true;
+            this.rbLine.Location = new System.Drawing.Point(76, 12);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(53, 20);
+            this.rbLine.TabIndex = 1;
+            this.rbLine.Text = "Line";
+            this.rbLine.UseVisualStyleBackColor = true;
+            // 
+            // rbPolygon
+            // 
+            this.rbPolygon.AutoSize = true;
+            this.rbPolygon.Location = new System.Drawing.Point(135, 12);
+            this.rbPolygon.Name = "rbPolygon";
+            this.rbPolygon.Size = new System.Drawing.Size(78, 20);
+            this.rbPolygon.TabIndex = 2;
+            this.rbPolygon.Text = "Polygon";
+            this.rbPolygon.UseVisualStyleBackColor = true;
+            // 
+            // dtgv
+            // 
+            this.dtgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Location = new System.Drawing.Point(13, 39);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.ReadOnly = true;
+            this.dtgv.RowHeadersWidth = 51;
+            this.dtgv.RowTemplate.Height = 24;
+            this.dtgv.Size = new System.Drawing.Size(276, 399);
+            this.dtgv.TabIndex = 3;
+            this.dtgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgv_CellClick);
+            // 
+            // panelMap
+            // 
+            this.panelMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMap.Bearing = 0F;
+            this.panelMap.CanDragMap = true;
+            this.panelMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.panelMap.GrayScaleMode = false;
+            this.panelMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.panelMap.LevelsKeepInMemmory = 5;
+            this.panelMap.Location = new System.Drawing.Point(295, 12);
+            this.panelMap.MarkersEnabled = true;
+            this.panelMap.MaxZoom = 2;
+            this.panelMap.MinZoom = 2;
+            this.panelMap.MouseWheelZoomEnabled = true;
+            this.panelMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.panelMap.Name = "panelMap";
+            this.panelMap.NegativeMode = false;
+            this.panelMap.PolygonsEnabled = true;
+            this.panelMap.RetryLoadTile = 0;
+            this.panelMap.RoutesEnabled = true;
+            this.panelMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.panelMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.panelMap.ShowTileGridLines = false;
+            this.panelMap.Size = new System.Drawing.Size(493, 426);
+            this.panelMap.TabIndex = 4;
+            this.panelMap.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelMap);
+            this.Controls.Add(this.dtgv);
+            this.Controls.Add(this.rbPolygon);
+            this.Controls.Add(this.rbLine);
+            this.Controls.Add(this.rbPoint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbPoint;
+        private System.Windows.Forms.RadioButton rbLine;
+        private System.Windows.Forms.RadioButton rbPolygon;
+        private System.Windows.Forms.DataGridView dtgv;
+        private GMap.NET.WindowsForms.GMapControl panelMap;
+    }
+}
+
---
+++ Job28_Read spatial data from PostGreSQL (points, lines, regions) and display on Winform map/Job28/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+using Npgsql;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace Job28
+{
+    public partial class Form1 : Form
+    {
+        private GMapControl map;
+        private string connectionString = "Host=localhost;Port=5432;Database=NganGiangMap;Username=postgres;Password=123;";
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMap();
+        }
+
+        private void InitializeMap()
+        {
+            // Khởi tạo control GMapControl
+            map = new GMapControl();
+
+            // Đặt thuộc tính Dock của GMapControl thành Fill để nó lấp đầy Panel1
+            map.Dock = DockStyle.Fill;
+
+            //map.MapProvider = GoogleMapProvider.Instance;
+            map.SetPositionByKeywords("Cameroon"); // Đặt vị trí mặc định của bản đồ
+            GMaps.Instance.Mode = AccessMode.ServerOnly; // Thiết lập bản đồ truy cập là máy chủ không phải máy khách
+
+            map.MapProvider = GMapProviders.GoogleMap;
+            //map.MapProvider = GMapProviders.GoogleSatelliteMap;
+            //map.Manager.Mode = AccessMode.ServerOnly; // Đảm bảo rằng bạn sử dụng chế độ server để có thể hiển thị bản đồ giao thông
+
+            panelMap.Controls.Add(map);
+
+            map.MinZoom = 1;
+            map.MaxZoom = 20;
+            map.Zoom = 6;
+        }
+
+        
+
+        private void AddLine(string connectionString, string searchItem)
+        {
+            map.Overlays.Clear();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT ST_ASTEXT(cmr_roads.geom) " +
+                               "FROM cmr_roads " +
+                               "WHERE EXISTS (" +
+                                    "SELECT 1 FROM cmr_adm1 " +
+                                    "WHERE ST_Within(cmr_roads.geom, cmr_adm1.geom) " +
+                                    "AND cmr_adm1.name_1 = @searchItem " +
+                               ");";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@searchItem", searchItem);
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // Đối tượng WKTReader từ thư viện GeoAPI (chuyển đổi dữ liệu địa lý từ dạng văn bản định dạng
+                        // (WKT - Well-Known Text) sang các đối tượng hình học trong mã lập trình.
+                        // Trong trường hợp này, nó được sử dụng để đọc dữ liệu văn bản định dạng WKT của các hình học từ cơ sở dữ liệu.
+                        var geomReader = new WKTReader();
+                        GMapOverlay overlay = new GMapOverlay("my_overlay");
+
+                        while (reader.Read())
+                        {
+                            string geomText = reader.GetString(0);
+                            Geometry geom = geomReader.Read(geomText);
+
+                            if (geom is MultiLineString multiLineString)
+                            {
+                                foreach (LineString lineString in multiLineString.Geometries)
+                                {
+                                    List<PointLatLng> points = GetPointsFromLineString(lineString);
+                                    GMapRoute route = new GMapRoute(points, "A road");
+                                    overlay.Routes.Add(route);
+                                }
+                            }
+                        }
+
+                        map.Overlays.Add(overlay);
+                        map.SetPositionByKeywords("Cameroon");
+                        map.Position = map.Position;
+                    }
+                }
+            }
+        }
+
+        private List<PointLatLng> GetPointsFromLineString(LineString lineString)
+        {
+            List<PointLatLng> points = new List<PointLatLng>();
+
+            foreach (Coordinate coordinate in lineString.Coordinates)
+            {
+                points.Add(new PointLatLng(coordinate.Y, coordinate.X)); // Swap X and Y for GMap compatibility
+            }
+
+            return points;
+        }
+
+        private void DisplayDataOnMap(string searchItem)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT ST_AsText(geom) AS geom_text FROM cmr_adm1 " +
+                               "WHERE name_1 = @searchItem";
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@searchItem", searchItem);
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var geomReader = new WKTReader();
+                            GMapOverlay overlay = new GMapOverlay("my_overlay");
+
+                            string geomText = reader.GetString(0);
+                            Geometry geom = geomReader.Read(geomText);
+
+                            if (geom is MultiPolygon multiPolygon)
+                            {
+                                foreach (Polygon polygon in multiPolygon.Geometries)
+                                {
+                                    List<PointLatLng> points = GetPointsFromPolygon(polygon);
+
+                                    if (rbPoint.Checked)
+                                    {
+                                        map.Overlays.Clear();
+                                        PointLatLng center = GetCenterPoint(points);
+                                        GMapMarker marker = new GMarkerGoogle(center, GMarkerGoogleType.red_dot);
+                                        GMapOverlay markersOverlay = new GMapOverlay("markers");
+                                        markersOverlay.Markers.Add(marker);
+                                        map.Overlays.Add(markersOverlay);
+                                    }
+
+                                    if (rbPolygon.Checked)
+                                    {
+                                        map.Overlays.Clear();
+                                        GMapPolygon gmapPolygon = new GMapPolygon(points, "Polygon");
+                                        overlay.Polygons.Add(gmapPolygon);
+                                    }
+                                }
+
+                                map.Overlays.Add(overlay);
+                                map.SetPositionByKeywords(searchItem + ", Japan");
+                                map.Position = map.Position;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<PointLatLng> GetPointsFromPolygon(Polygon polygon)
+        {
+            List<PointLatLng> points = new List<PointLatLng>();
+
+            foreach (Coordinate coordinate in polygon.Coordinates)
+            {
+                points.Add(new PointLatLng(coordinate.Y, coordinate.X)); // Swap X and Y for GMap compatibility
+            }
+
+            return points;
+        }
+
+        private PointLatLng GetCenterPoint(List<PointLatLng> points)
+        {
+            double totalLat = 0;
+            double totalLng = 0;
+
+            foreach (var point in points)
+            {
+                totalLat += point.Lat;
+                totalLng += point.Lng;
+            }
+
+            double avgLat = totalLat / points.Count;
+            double avgLng = totalLng / points.Count;
+
+            return new PointLatLng(avgLat, avgLng);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string query = "SELECT name_1, hasc_1 FROM cmr_adm1";
+            DataTable dt = new DataTable();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+                    dtgv.DataSource = dt;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Giải phóng bản đồ khi form đóng
+            if (map != null)
+            {
+                map.Dispose();
+            }
+        }
+
+        private void dtgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem ô đã bấm có phải là ô dữ liệu không và không phải là ô header
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                object cellValue = dtgv.Rows[e.RowIndex].Cells[0].Value;
+
+                // Kiểm tra giá trị của ô không rỗng
+                if (cellValue != null)
+                {
+                    string searchItem = cellValue.ToString();
+
+                    // Kiểm tra xem người dùng đã chọn loại dữ liệu nào để hiển thị trên bản đồ
+                    if (rbPolygon.Checked || rbPoint.Checked)
+                        DisplayDataOnMap(searchItem);
+
+                    if (rbLine.Checked)
+                        AddLine(connectionString, searchItem);
+                }
+            }
+        }
+    }
+}
