94f1479110560c8fe6d01701f5112245cabe5e05
DungSatoru <dungsatoru.dev@gmail.com>
2024-04-27 15:42:20 +0700

    Job27_Draw the Point, Line and Polygon on Map

---
+++ Job27_Draw the Point, Line and Polygon on Map/View Map on Winform/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace View_Map_on_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.latitude = new System.Windows.Forms.TextBox();
+            this.longitude = new System.Windows.Forms.TextBox();
+            this.goCordinates = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.zoomLevel = new System.Windows.Forms.NumericUpDown();
+            this.btnReturnCordinate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFlagThePoint = new System.Windows.Forms.Button();
+            this.btnAddPolygon = new System.Windows.Forms.Button();
+            this.btnCreateLine = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // latitude
+            // 
+            this.latitude.Location = new System.Drawing.Point(88, 16);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(162, 22);
+            this.latitude.TabIndex = 0;
+            // 
+            // longitude
+            // 
+            this.longitude.Location = new System.Drawing.Point(88, 44);
+            this.longitude.Name = "longitude";
+            this.longitude.Size = new System.Drawing.Size(162, 22);
+            this.longitude.TabIndex = 1;
+            // 
+            // goCordinates
+            // 
+            this.goCordinates.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.goCordinates.Location = new System.Drawing.Point(12, 72);
+            this.goCordinates.Name = "goCordinates";
+            this.goCordinates.Size = new System.Drawing.Size(238, 49);
+            this.goCordinates.TabIndex = 0;
+            this.goCordinates.Text = "Go to Cordinates";
+            this.goCordinates.UseVisualStyleBackColor = true;
+            this.goCordinates.Click += new System.EventHandler(this.goCordinates_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(267, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(521, 419);
+            this.panel1.TabIndex = 5;
+            // 
+            // zoomLevel
+            // 
+            this.zoomLevel.Location = new System.Drawing.Point(125, 144);
+            this.zoomLevel.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.zoomLevel.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.zoomLevel.Name = "zoomLevel";
+            this.zoomLevel.Size = new System.Drawing.Size(125, 22);
+            this.zoomLevel.TabIndex = 6;
+            this.zoomLevel.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.zoomLevel.ValueChanged += new System.EventHandler(this.zoomLevel_ValueChanged);
+            // 
+            // btnReturnCordinate
+            // 
+            this.btnReturnCordinate.Location = new System.Drawing.Point(12, 188);
+            this.btnReturnCordinate.Name = "btnReturnCordinate";
+            this.btnReturnCordinate.Size = new System.Drawing.Size(238, 49);
+            this.btnReturnCordinate.TabIndex = 7;
+            this.btnReturnCordinate.Text = "Active Cordinates Click";
+            this.btnReturnCordinate.UseVisualStyleBackColor = true;
+            this.btnReturnCordinate.Click += new System.EventHandler(this.btnReturnCordinate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Change zoom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Latitude";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Longitude";
+            // 
+            // btnFlagThePoint
+            // 
+            this.btnFlagThePoint.Location = new System.Drawing.Point(12, 243);
+            this.btnFlagThePoint.Name = "btnFlagThePoint";
+            this.btnFlagThePoint.Size = new System.Drawing.Size(116, 49);
+            this.btnFlagThePoint.TabIndex = 11;
+            this.btnFlagThePoint.Text = "Add Marker";
+            this.btnFlagThePoint.UseVisualStyleBackColor = true;
+            this.btnFlagThePoint.Click += new System.EventHandler(this.btnFlagThePoint_Click);
+            // 
+            // btnAddPolygon
+            // 
+            this.btnAddPolygon.Location = new System.Drawing.Point(12, 298);
+            this.btnAddPolygon.Name = "btnAddPolygon";
+            this.btnAddPolygon.Size = new System.Drawing.Size(238, 49);
+            this.btnAddPolygon.TabIndex = 13;
+            this.btnAddPolygon.Text = "Draw the Polygon";
+            this.btnAddPolygon.UseVisualStyleBackColor = true;
+            this.btnAddPolygon.Click += new System.EventHandler(this.btnAddPolygon_Click);
+            // 
+            // btnCreateLine
+            // 
+            this.btnCreateLine.Location = new System.Drawing.Point(134, 243);
+            this.btnCreateLine.Name = "btnCreateLine";
+            this.btnCreateLine.Size = new System.Drawing.Size(116, 49);
+            this.btnCreateLine.TabIndex = 12;
+            this.btnCreateLine.Text = "Draw the Line";
+            this.btnCreateLine.UseVisualStyleBackColor = true;
+            this.btnCreateLine.Click += new System.EventHandler(this.btnCreateLine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddPolygon);
+            this.Controls.Add(this.btnCreateLine);
+            this.Controls.Add(this.btnFlagThePoint);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReturnCordinate);
+            this.Controls.Add(this.zoomLevel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.goCordinates);
+            this.Controls.Add(this.longitude);
+            this.Controls.Add(this.latitude);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.zoomLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox latitude;
+        private System.Windows.Forms.TextBox longitude;
+        private System.Windows.Forms.Button goCordinates;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown zoomLevel;
+        private System.Windows.Forms.Button btnReturnCordinate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnFlagThePoint;
+        private System.Windows.Forms.Button btnAddPolygon;
+        private System.Windows.Forms.Button btnCreateLine;
+    }
+}
+
---
+++ Job27_Draw the Point, Line and Polygon on Map/View Map on Winform/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace View_Map_on_Winform
+{
+    public partial class Form1 : Form
+    {
+        private GMapControl map;
+        private int defaultZoom = 10;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Khởi tạo control GMapControl
+            map = new GMapControl();
+
+            // Đặt thuộc tính Dock của GMapControl thành Fill để nó lấp đầy Panel1
+            map.Dock = DockStyle.Fill;
+
+            // Thêm GMapControl vào Panel1
+            panel1.Controls.Add(map);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Thiết lập nhà cung cấp bản đồ và tọa độ ban đầu của GMapControl
+            //map.MapProvider = GMapProviders.GoogleMap;
+            // Đặt kiểu bản đồ thành ảnh vệ tinh
+            // map.MapProvider = GMapProviders.GoogleSatelliteMap;
+            // Đặt kiểu bản đồ thành giao thông
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.Manager.Mode = AccessMode.ServerOnly; // Đảm bảo rằng bạn sử dụng chế độ server để có thể hiển thị bản đồ giao thông
+
+
+            map.Position = new PointLatLng(21.0285, 105.8542); // Set initial position
+            map.MinZoom = 1;
+            map.MaxZoom = 20;
+            map.Zoom = defaultZoom;
+
+            latitude.Text = (21.0285).ToString();
+            longitude.Text = (105.8542).ToString();
+        }
+
+        private void goCordinates_Click(object sender, EventArgs e)
+        {
+            map.Position = new PointLatLng(Convert.ToDouble(latitude.Text), Convert.ToDouble(longitude.Text));
+            map.Zoom = defaultZoom;
+            map.Update();
+            map.Refresh();
+        }
+
+
+        private void zoomLevel_ValueChanged(object sender, EventArgs e)
+        {
+            map.Zoom = Convert.ToDouble(zoomLevel.Value);
+            map.Update();
+            map.Refresh();
+        }
+
+        void gmap_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Lấy tọa độ của điểm được nhấp chuột trên bản đồ
+            PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+
+            // Hiển thị tọa độ trên TextBox latitude và longitude
+            latitude.Text = point.Lat.ToString();
+            longitude.Text = point.Lng.ToString();
+        }
+
+        private bool isActive = false;
+        private void btnReturnCordinate_Click(object sender, EventArgs e)
+        {
+            if (!isActive)
+            {
+                // Gắn sự kiện MouseClick cho control GMapControl
+                map.MouseClick += gmap_MouseClick;
+                btnReturnCordinate.Text = "Unactive Cordinates Click";
+                isActive = true;
+            }
+            else
+            {
+                // Hủy bỏ sự kiện MouseClick từ control GMapControl
+                map.MouseClick -= gmap_MouseClick;
+                btnReturnCordinate.Text = "Active Cordinates Click";
+                isActive = false;
+            }
+        }
+
+        void map_AddMarker_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Lấy tọa độ của điểm được nhấp chuột trên bản đồ
+                PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+
+                double currentZoom = map.Zoom;
+
+                map.Zoom = currentZoom;
+
+                GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.blue_dot);
+
+                // 1. Tạo 1 lớp phủ
+                GMapOverlay markers = new GMapOverlay("markers");
+
+                // 2. Thêm tất cả các điểm đánh dấu có sẵn vào Lớp phủ đó
+                markers.Markers.Add(marker);
+
+                // 3. Che phủ bản đồ bằng Overlay
+                map.Overlays.Add(markers);
+
+                // Đảm bảo rằng marker được hiển thị ở vị trí được click
+                PointLatLng center = map.Position;
+                map.Position = center;
+            }
+        }
+
+
+        private bool isActiveAddMarker = false;
+
+        private void btnFlagThePoint_Click(object sender, EventArgs e)
+        {
+            if (!isActiveAddMarker)
+            {
+                // Gắn sự kiện MouseClick cho control GMapControl
+                map.MouseClick += map_AddMarker_MouseClick;
+                btnFlagThePoint.Text = "Unactive Add Markers";
+                isActiveAddMarker = true;
+            }
+            else
+            {
+                // Hủy bỏ sự kiện MouseClick từ control GMapControl
+                map.MouseClick -= map_AddMarker_MouseClick;
+                btnFlagThePoint.Text = "Flag the point";
+                isActiveAddMarker = false;
+                map.Overlays.Clear(); // Xóa toàn bộ các lớp phủ, bao gồm cả các điểm đánh dấu
+                map.Refresh();
+            }
+        }
+
+        bool isCreatingLine = false;
+        PointLatLng startPoint = new PointLatLng();
+        List<PointLatLng> linePoints = new List<PointLatLng>();
+        private void btnCreateLine_Click(object sender, EventArgs e)
+        {
+            // Chuyển đổi trạng thái của biến isCreatingLine giữa true và false
+            isCreatingLine = !isCreatingLine;
+
+            // Cập nhật văn bản của nút tùy thuộc vào trạng thái của biến isCreatingLine
+            btnCreateLine.Text = isCreatingLine ? "Unactive Draw the Line" : "Draw the Line";
+
+            if (isCreatingLine)
+            {
+                // Bắt đầu xử lý sự kiện MouseClick khi chức năng vẽ đường được kích hoạt
+                map.MouseClick += map_AddMarker_MouseClick;
+                map.MouseClick += map_AddLine_MouseClick;
+            }
+            else
+            {
+                // Ngừng xử lý sự kiện MouseClick khi chức năng vẽ đường được tắt
+                map.MouseClick -= map_AddMarker_MouseClick;
+                map.MouseClick -= map_AddLine_MouseClick;
+                // Reset danh sách các điểm và điểm bắt đầu
+                linePoints = null;
+                startPoint = new PointLatLng();
+                map.Overlays.Clear(); // Xóa toàn bộ các lớp phủ, bao gồm cả các điểm đánh dấu
+                map.Refresh();
+            }
+        }
+
+        private void map_AddLine_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Lấy tọa độ của điểm được nhấp chuột trên bản đồ
+            PointLatLng point = map.FromLocalToLatLng(e.X, e.Y);
+
+            // Nếu danh sách các điểm tạo thành đường chưa được khởi tạo, khởi tạo nó
+            if (linePoints == null)
+            {
+                linePoints = new List<PointLatLng>();
+            }
+
+            // Thêm điểm vào danh sách
+            linePoints.Add(point);
+
+
+            // Nếu chỉ có một điểm, lưu lại điểm đó và không vẽ đường
+            if (linePoints.Count == 1)
+            {
+                startPoint = point;
+                return;
+            }
+
+            // Tạo đường từ danh sách các điểm
+            GMapRoute route = new GMapRoute(linePoints, "MyRoute");
+
+            // Tạo lớp phủ để chứa đường
+            GMapOverlay routesOverlay = new GMapOverlay("routes");
+            routesOverlay.Routes.Add(route);
+
+            // Thêm lớp phủ vào bản đồ
+            map.Overlays.Add(routesOverlay);
+
+            // Đảm bảo rằng marker được hiển thị ở vị trí được click
+            PointLatLng center = map.Position;
+            map.Position = center;
+        }
+
+        private List<PointLatLng> polygonPoints = new List<PointLatLng>();
+
+        private void map_AddPolygon_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Lấy tọa độ của điểm được click trên bản đồ
+            PointLatLng clickedPoint = map.FromLocalToLatLng(e.X, e.Y);
+
+            // Thêm điểm vào danh sách các điểm của đa giác
+            polygonPoints.Add(clickedPoint);
+
+            // Kiểm tra nếu đã có đủ điểm để tạo đa giác
+            if (polygonPoints.Count >= 3)
+            {
+                // Tạo đa giác mới từ danh sách các điểm đã chọn
+                var polygon = new GMapPolygon(polygonPoints, "My Area")
+                {
+                    Stroke = new Pen(Color.DarkGreen, 2),
+                    Fill = new SolidBrush(Color.BurlyWood)
+                };
+
+                // Tạo một Overlay mới để chứa đa giác và thêm nó vào bản đồ
+                var polygonsOverlay = new GMapOverlay("polygons");
+                polygonsOverlay.Polygons.Add(polygon);
+                map.Overlays.Add(polygonsOverlay);
+
+            }
+            // Đảm bảo rằng marker được hiển thị ở vị trí được click
+            PointLatLng center = map.Position;
+            map.Position = center;
+        }
+
+        bool isCreatingPolygon = false;
+
+        private void btnAddPolygon_Click(object sender, EventArgs e)
+        {
+            isCreatingPolygon = !isCreatingPolygon;
+            btnAddPolygon.Text = isCreatingPolygon ? "Unactive Draw the Polygon" : "Draw the Polygon";
+
+
+            if (isCreatingPolygon)
+            {
+                // Bắt đầu xử lý sự kiện MouseClick khi chức năng vẽ đường được kích hoạt
+                map.MouseClick += map_AddPolygon_MouseClick;
+            }
+            else
+            {
+                // Ngừng xử lý sự kiện MouseClick khi chức năng vẽ đường được tắt
+                map.MouseClick -= map_AddPolygon_MouseClick;
+                // Reset danh sách các điểm và điểm bắt đầu
+                polygonPoints.Clear();
+                map.Overlays.Clear(); // Xóa toàn bộ các lớp phủ, bao gồm cả các điểm đánh dấu
+                map.Refresh();
+            }
+            // MessageBox.Show("Nhấp chuột trên bản đồ để chọn các điểm của đa giác. Khi hoàn thành, nhấn nút Lưu.", "Hướng dẫn", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Giải phóng tài nguyên của GMapControl
+            map.Dispose();
+        }
+    }
+}
