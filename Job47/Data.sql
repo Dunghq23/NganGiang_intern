528e7e012ed9fe23b17f98bc4231b1b38b1c2409
Dunghq23 <dungha.4work@gmail.com>
2024-08-16 18:06:22 +0700

    Done Job 46 & 47

---
+++ Job47/Data.sql
@@ -0,0 +1,61 @@
+﻿USE TimeKeeping;
+
+INSERT INTO departments ([department_name]) values
+(N'Công nghệ thông tin'), 
+(N'Kinh doanh'), 
+(N'Tài chính - Kế toán'), 
+('Marketing');
+
+SELECT * FROM departments;
+--DELETE FROM Departments;
+
+--GO
+--DBCC CHECKIDENT ('Departments', RESEED, 0);
+--GO
+
+--SELECT @@SERVERNAME; -- get your server name
+
+--ALTER AUTHORIZATION ON DATABASE::[TimeKeeping] TO [REALTUN\Administrator];
+
+--INSERT INTO Employees ([employee_name], [fk_department_id]) values
+--(N'Đỗ Hữu Tuấn', 1), (N'Lê Đình Tú', 1), (N'Nguyễn Kim Thi', 1), (N'Hạ Quang Dũng', 1), 
+--(N'Phạm Trọng Nghĩa', 1), (N'Đặng Hải Sơn', 1), (N'Lê Thu Trang', 1), (N'Phạm Quỳnh Anh', 1);
+
+SELECT * FROM employees;
+
+
+
+INSERT INTO employees 
+(
+    employee_username, 
+    employee_name, 
+    email, 
+    phone_number, 
+    address, 
+    date_of_birth, 
+    gender, 
+    position, 
+    fk_department_id, 
+    start_date, 
+    salary, 
+    employment_status, 
+    profile_picture, 
+    notes
+)
+VALUES 
+(
+    N'dunghq23', 
+    N'Hạ Quang Dũng', 
+    'haquangdung18092003@gmail.com', 
+    N'0393049255', 
+    N'Tổ 8 - Thị trấn Chi Đông - Mê Linh - Hà Nội', 
+    '2003-09-18', 
+    1, 
+    N'Thực tập sinh', 
+    1, 
+    '2023-09-15', 
+    100.00, 
+    1, 
+    N'https://absvietnam.com/wp-content/uploads/2024/03/Nhat-Ban-dat-nuoc-mat-troi-moc-1.jpg', 
+    N'Thực tập mảng .NET, AI, Web, Android,..'
+)
---
+++ Job47/init.sql
@@ -0,0 +1,51 @@
+﻿CREATE DATABASE TimeKeeping;
+GO
+
+USE TimeKeeping;
+GO
+
+CREATE TABLE departments
+(
+	[department_id] INT PRIMARY KEY IDENTITY(1, 1),
+	[department_name] NVARCHAR(100) UNIQUE not null
+)
+GO
+
+CREATE TABLE employees
+(
+    [employee_id] INT PRIMARY KEY IDENTITY(1, 1), -- Khóa chính
+	[employee_username] VARCHAR (20) NOT NULL, -- Username của nhân viên 
+    [employee_name] NVARCHAR(100) NOT NULL, -- Họ và tên nhân viên
+    [email] NVARCHAR(100) NOT NULL UNIQUE, -- Email
+    [phone_number] NVARCHAR(15), -- Số điện thoại
+    [address] NVARCHAR(255), -- Địa chỉ
+    [date_of_birth] DATE, -- Ngày sinh
+    [gender] BIT, -- Giới tính [Nữ: 0; Nam: 1]
+    [position] NVARCHAR(50), -- Chức vụ
+    [fk_department_id] INT NOT NULL, -- Phòng ban (liên kết với bảng departments)
+    [start_date] DATE, -- Ngày bắt đầu làm việc
+    [salary] DECIMAL(18, 2), -- Mức lương
+    [employment_status] BIT, -- Trạng thái công việc [Đã nghỉ việc: 0; Đang làm việc]
+    [profile_picture] NVARCHAR(255), -- Đường dẫn ảnh đại diện
+    [notes] NVARCHAR(MAX), -- Ghi chú
+    [created_at] DATETIME DEFAULT GETDATE(), -- Ngày tạo
+    [updated_at] DATETIME DEFAULT GETDATE(), -- Ngày cập nhật lần cuối
+    
+    -- Khóa ngoại
+    FOREIGN KEY (fk_department_id) REFERENCES departments(department_id)
+)
+GO
+
+
+CREATE TABLE timekeeping
+(
+    [timekeeping_id] INT PRIMARY KEY IDENTITY(1, 1),
+    [fk_employee_id] INT NOT NULL,
+    [check_in] DATETIME DEFAULT CAST(SWITCHOFFSET(SYSDATETIMEOFFSET(), '+07:00') AS DATETIME),
+    [check_out] DATETIME,
+    FOREIGN KEY (fk_employee_id) REFERENCES employees(employee_id)
+);
+GO
+
+
+select * from employees
